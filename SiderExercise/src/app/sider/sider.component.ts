3cd5988207916564c1d767d8d83f7bc16cf2e8d4
Andrew Yang <zongyaoyang@Andrews-MacBook-Air.local>
2021-11-14 13:37:53 -0500

    slider done

--- SiderExercise/src/app/sider/sider.component.ts
+++ SiderExercise/src/app/sider/sider.component.ts
@@ -6,7 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sider.component.css']
 })
 export class SiderComponent implements OnInit {
-  imageSize: number = 100;
+  imageSize: number;
+  x: number = 0;
+  isChanging: boolean = false;
 
   constructor() { }
 
@@ -15,6 +17,43 @@ export class SiderComponent implements OnInit {
 
   onChange(event) {
     this.imageSize = event.value;
+    console.log(event);
+  }
+  onInput(event) {
+    this.imageSize = event.value;
+    console.log(event.value);
+  }
+
+  onMousedown(event) {
+    this.x = event.offsetX;
+    this.isChanging = true;
+  }
+
+  onMousemove(event) {
+    if (this.isChanging === true) {
+      this.mouseEventControl(this.x, event.offsetX);
+      this.x = event.offsetX;
+    }
+  }
+
+  onMouseup(event) {
+    if (this.isChanging === true) {
+      this.mouseEventControl(this.x, event.offsetX);
+      this.x = 0;
+      this.isChanging = false;
+    }
+  }
+
+  mouseEventControl(x1, x2) {
+    let change = x2 - x1
+    if (change > 200 - this.imageSize) {
+      this.imageSize = 200;
+    } 
+    if (this.imageSize + change < 1) {
+      this.imageSize = 1
+    }
+    this.imageSize = this.imageSize + change;
+
   }
 
 }
